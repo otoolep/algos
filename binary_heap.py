0bbbbd540dc9240c3d1224eb206a4e4e1b45ef59
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-14 20:07:38 -0700

    Add binary heap Python implementation

---
+++ binary_heap.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+class MinHeap(object):
+    def __init__(self):
+        # Create heap with empty slot at 0, to simplify math.
+        self.heap = [0]
+        self.current_size = 0
+
+    def Insert(self, n):
+        self.heap.append(n)
+        self.current_size += 1
+        self.percUp(self.current_size)
+
+    def percUp(self, i):
+        # Keep going until the root node.
+        while i // 2 >= 1:
+            # Check if parent node is smaller.
+            parent = i/2
+            if self.heap[parent] > self.heap[i]:
+                tmp = self.heap[parent]
+                self.heap[parent] = self.heap[i]
+                self.heap[i] = tmp
+
+            # Go up a level and check again.
+            i = i / 2
+
+    def __str__(self):
+        return 'Heap: %s' % str(self.heap[1:])
+
+h = MinHeap()
+
+for n in [4,7,6,8,2,4,6,8,2,5,1]:
+    print 'Inserting %d' % n
+    h.Insert(n)
+    print h
+    print
