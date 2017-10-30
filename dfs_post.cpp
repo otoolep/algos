81f6e610055d1a2250deec5c91cbde7252776b99
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-29 22:16:27 -0700

    Add some DFS algos

---
+++ dfs_post.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct element_t {
+	int value;
+	element_t* left;
+	element_t* right;
+} element;
+
+void dfs_in(element *elem) {
+    if (!elem) {
+        return;
+    }
+
+    dfs_in(elem->left);
+    dfs_in(elem->right);
+    cout << elem->value << " ";
+}
+
+element* newElem(int value, element* left, element* right) {
+	element* e = (element*) malloc(sizeof(element));
+	e->value = value;
+	e->left = left;
+	e->right = right;
+	return e;
+}
+
+int main() {
+	// Create a BST.
+	//
+	//           5
+    //         4   7
+    //       1   6   9
+
+	element* one = newElem(1, NULL, NULL);
+	element* six = newElem(6, NULL, NULL);
+	element* nine = newElem(9, NULL, NULL);
+	element* four = newElem(4, one, NULL);
+	element* seven = newElem(7, six, nine);
+	element* five = newElem(5, four, seven);
+
+	dfs_in(five);
+    cout << endl;
+	return 0;
+}
---
+++ dfs_pre.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+typedef struct element_t {
+	int value;
+	element_t* left;
+	element_t* right;
+} element;
+
+void dfs_pre(element *elem) {
+	stack<element*> s;
+	s.push(elem);
+
+	while (!s.empty()) {
+		element* e = s.top();
+		s.pop();
+		if (!e) {
+			continue;
+		}
+		cout << e->value << " ";
+
+		s.push(e->left);
+		s.push(e->right);
+	}
+	cout << endl;
+}
+
+element* newElem(int value, element* left, element* right) {
+	element* e = (element*) malloc(sizeof(element));
+	e->value = value;
+	e->left = left;
+	e->right = right;
+	return e;
+}
+
+int main() {
+	// Create a tree.
+	//
+	//           5
+    //         4   7
+    //       1  6   9
+
+	element* one = newElem(1, NULL, NULL);
+	element* six = newElem(6, NULL, NULL);
+	element* nine = newElem(9, NULL, NULL);
+	element* four = newElem(4, one, six);
+	element* seven = newElem(7, NULL, nine);
+	element* five = newElem(5, four, seven);
+
+	dfs_pre(five);
+	return 0;
+}
