4404d2967a96d59ca7f3b165d57c80beb58130c1
Philip O'Toole <philip.otoole@yahoo.com>
2017-11-03 09:45:52 -0700

    Many fixes, still debugging

--- bits/smallest_largest.c
+++ bits/smallest_largest.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 int COUNTS[256];
 
@@ -7,7 +8,7 @@ int count_bits_byte(unsigned char b) {
     unsigned char m = 0x1;
     for (int n = 0; n < 8; n++) {
         if (b & m) c++;
-        m << 1;
+        m = m << 1;
     }
     return c;
 }
@@ -15,27 +16,41 @@ int count_bits_byte(unsigned char b) {
 int count_bits_int(unsigned int i) {
     int c = 0;
     for (int n = 0; n < 4; n++) {
+        printf("%i %i\n", (unsigned char)i, COUNTS[(unsigned char)i]);
         c += COUNTS[(unsigned char)i];
-        i << 8;
+        printf("1: 0x%x\n", i);
+        i = i << 8;
+        printf("2: 0x%x\n", i);
     }
     return c;
 }
 
 
 void create_lookup() {
-    for (int i = 0; i < 256; i++) {
-        COUNTS[i] = count_bits_byte((unsigned char)i);
+    COUNTS[0] = 0;
+
+    for (unsigned char i = 1; i > 0; i++) {
+        COUNTS[i] = count_bits_byte(i);
     }
 }
 
 int main() {
-    int a = 1755;
+    int a = 2;
     int max = a;
     int min = a;
 
     create_lookup();
+    assert(COUNTS[1] == 1);
+    assert(COUNTS[2] == 1);
+    assert(COUNTS[3] == 2);
 
-    int ca = count_bits_int(a);
+    //int ca = count_bits_int(a);
+    int ca;
+    //assert(ca = 1);
+
+    //assert(count_bits_int(0xFF) == 8);
+    assert(count_bits_int(0x00FF0000) == 8);
+    assert(1==2);
 
     for (int i = 0; max == a || min == a; i++)  {
         if (max == a) {
