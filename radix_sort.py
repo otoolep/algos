57dcc6e1931504265c98a97cac72af34d6113fa6
Philip O Toole <philip.otoole@yahoo.com>
2017-11-20 09:26:13 -0500

    Add radix sort

---
+++ radix_sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Radix sort from Wikipedia.
+
+def radix_sort(array, base=10):
+    def list_to_buckets(array, base, iteration):
+        buckets = [[] for _ in range(base)]
+        for number in array:
+            # Isolate the base-digit from the number
+            digit = (number // (base ** iteration)) % base
+            # Drop the number into the correct bucket
+            buckets[digit].append(number)
+        return buckets
+
+    def buckets_to_list(buckets):
+        return [x for bucket in buckets for x in bucket]
+
+    maxval = max(array)
+
+    it = 0
+    # Iterate, sorting the array by each base-digit
+    while base ** it <= maxval:
+        array = buckets_to_list(list_to_buckets(array, base, it))
+        it += 1
+
+    return array
+
+array = [43,57,29,23,1,0,890,22,771]
+
+print array
+print radix_sort(array)
